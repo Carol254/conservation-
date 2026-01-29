e6c3247974028cb911d324388d9e8d7cb215e52b
Carol254 <wanguic49@gmail.com>
2026-01-29 19:33:55 +0300

    added the gallery component path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Gallery } from './gallery/gallery';
 
 export const routes: Routes = [
     {
         path:'',
         component:Home
+    },
+    {
+        path:'/gallery',
+        component:Gallery
     }
 
 ];
