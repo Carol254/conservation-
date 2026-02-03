319dac2e2cb4768c0d52d7fb156d367c1c4e42d0
Carol254 <wanguic49@gmail.com>
2026-02-03 14:59:00 +0300

    added changes to the stories component

--- src/app/stories/stories.ts
+++ src/app/stories/stories.ts
@@ -2,22 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Observable,map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-stories',
-  imports: [MatButtonModule ,MatCardModule],
+  imports: [MatButtonModule ,MatCardModule ,AsyncPipe],
   templateUrl: './stories.html',
   styleUrl: './stories.css',
 })
 export class Stories  implements OnInit{
-  stories:any[]= [];
+  
 
   constructor(private http: HttpClient){}
+  stories$!: Observable<any[]>;
 
   ngOnInit(): void {
-    this.http.get<any>('assets/stories.json').subscribe(data =>{
-      this.stories = data.conservation_stories;
-    });
+   this.stories$ = this.http.get<any>('assets/stories.json').pipe(
+    map(data => data.conservation_stories)
+   );
   }
 
 }
