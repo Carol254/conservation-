f7cc5abe7bd77e080fb943cf232f06be0010a0e6
Carol254 <wanguic49@gmail.com>
2026-02-03 12:01:47 +0300

    added angular material

--- src/app/stories/stories.ts
+++ src/app/stories/stories.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-stories',
-  imports: [],
+  imports: [MatButtonModule ,MatCardModule],
   templateUrl: './stories.html',
   styleUrl: './stories.css',
 })
 export class Stories {
-    
+
 }
