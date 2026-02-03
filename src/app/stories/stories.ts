c20e56d8962e2a35fdd00748b62264aa71c58041
Carol254 <wanguic49@gmail.com>
2026-02-03 15:06:57 +0300

    changes to material card

--- src/app/stories/stories.ts
+++ src/app/stories/stories.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 import { Observable,map } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-stories',
-  imports: [MatButtonModule ,MatCardModule ,AsyncPipe],
+  imports: [MatButtonModule ,MatCardModule ,AsyncPipe ,MatIcon],
   templateUrl: './stories.html',
   styleUrl: './stories.css',
 })
