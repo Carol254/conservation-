5af89d4827901d76285f0fa178b69ddd04e3ad35
Carol254 <wanguic49@gmail.com>
2026-02-05 11:04:36 +0300

    changes to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Home } from './home/home';
 import { Gallery } from './gallery/gallery';
 import { Stories } from './stories/stories';
 import { About } from './about/about';
+import { Flora } from './flora/flora';
+import { Fauna } from './fauna/fauna';
 
 export const routes: Routes = [
     {
@@ -20,6 +22,14 @@ export const routes: Routes = [
     {
         path:'about',
         component:About
+    },
+    {
+        path:'flora',
+        component:Flora
+    },
+    {
+        path:'fauna',
+        component:Fauna
     }
 
 ];
