de23c5d5fc93b60c6fa9ba49eeaff50fabfc403d
Carol254 <wanguic49@gmail.com>
2026-02-09 10:47:58 +0300

    added images to fauna component

--- src/app/fauna/fauna.ts
+++ src/app/fauna/fauna.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { Observable,map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-fauna',
-  imports: [],
+  imports: [MatButtonModule,MatCardModule,AsyncPipe],
   templateUrl: './fauna.html',
   styleUrl: './fauna.css',
 })
-export class Fauna {
+export class Fauna  implements OnInit{
+   
+  constructor(private http:HttpClient){}
+  wildlife$!: Observable<any[]>;
 
+
+  ngOnInit(): void {
+    this.wildlife$ = this.http.get<any>('assets/wildlife_gallery.json').pipe(
+      map(data => data.wildlife_gallery)
+    );
+  }
 }
